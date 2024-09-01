bc5158c5a0f2e626fd735ab6fd2027c8ddad3fa5
Jinpisuth <164853102+Jinpisuth@users.noreply.github.com>
2024-09-01 09:15:56 +0700

    Update Healthguard-QQ.py

--- Healthguard-QQ.py
+++ Healthguard-QQ.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
-pip install matplotlib
+install matplotlib
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
